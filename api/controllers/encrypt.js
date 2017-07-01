6b76ba66a44a585ea7b136fb1bca019c7feb6726
TingRu.Chen <so910327@gmail.com>
2017-07-01 13:41:38 +0800

    swagger test

--- api/controllers/encrypt.js
+++ api/controllers/encrypt.js
@@ -1,39 +1,46 @@
-var app = require('../../app');
 var bodyParser = require('body-parser');
 var request = require('request');
 
-app.use(bodyParser.json());
-
 function check(str){
-  if(str.search(/[0-9+abcdef]/g) !== -1 ) return true;
-  return false;
+  if(str.search(/[^0-9a-f]/gi) > -1 ) return false;
+  return true;
 }
 
 function getvalue(req){
   req = JSON.stringify(req);
   var reqSth = {
     url : 'https://nkiua09s52.execute-api.ap-northeast-1.amazonaws.com/dev/encrypt',
-    headers:{
+    headers : {
       'content-type' : 'application/json'
     },
     body : req
   }
-  return request.post(reqSth, function(req, res, body){
-    console.log(body);
+  var require = request.post(reqSth, function(err, res){
+    var cipher = JSON.parse(res.body).ciphertext;
+    //res.send(res.body);
+    console.log(cipher);
+    return cipher;
   })
+  return require;
 }
 
-app.post('/encrypt', function encrypt(req, res){
+function encrypt(req, res){
   var plain = req.body.plaintext;
-  //console.log(req.headers);
+  //console.log(check(plain));
   if(req.headers['content-type'] !== 'application/json' || plain === undefined || check(plain) === false)
     res.json(400, {
       message: "給人看的錯誤說明",
     });
-  var ciphertext = getvalue({"plaintext" : plain});
-  //console.log(ciphertext);
+  var cipher =getvalue({"plaintext" : plain});
+  if(cipher.length >= 32)
+    res.json(413, {
+      message: "給人看的錯誤說明",
+    })
   res.json(200, {
-    ciphertext
+    "ciphertext" : cipher
   })
+}
 
-})
+module.exports = {
+  encrypt
+}
