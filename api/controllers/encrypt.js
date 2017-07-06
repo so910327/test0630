6143ef276b0e7fc79f25d4da63493a3b02f0e1a4
TingRu.Chen <so910327@gmail.com>
2017-07-06 14:14:36 +0800

    encrypt still has a problem

--- api/controllers/encrypt.js
+++ api/controllers/encrypt.js
@@ -2,44 +2,40 @@ var bodyParser = require('body-parser');
 var request = require('request');
 
 function check(str){
-  if(str.search(/[^0-9a-f]/gi) > -1 ) return false;
-  return true;
+  return (str.match(/[0-9a-fA-F]/) === null ) ? false : true;
 }
 
 function encrypt(req, res){
   var plain = req.body.plaintext;
   //console.log(check(plain));
-  if(req.headers['content-type'] !== 'application/json' || plain === undefined || check(plain) === false)
+  if(req.headers['content-type'] !== 'application/json' || !check(plain) || plain === undefined)
     return res.json(400, {
-      message: "給人看的錯誤說明",
+      "message": "Bad Request",
     });
-    var req = JSON.stringify({"plaintext" : plain});
-    var reqSth = {
-      url : 'https://nkiua09s52.execute-api.ap-northeast-1.amazonaws.com/dev/encrypt',
-      headers : {
-        'content-type' : 'application/json'
-      },
-      body : req
-    }
-    request.post(reqSth, function(err, response, body){
-      var cipher = JSON.parse(response.body).ciphertext;
-      console.log(cipher);
-      if(cipher === undefined)
-        res.json(400, {
-          message: "給人看的錯誤說明",
-        });
-      else{
-        if(cipher.length > 32)
-          res.json(413, {
-            message: "給人看的錯誤說明",
-          })
-        res.json(200, {
-          "ciphertext" : cipher
-        })
-      }
+  var req = JSON.stringify({"plaintext" : plain});
+  var reqSth = {
+    url : 'https://nkiua09s52.execute-api.ap-northeast-1.amazonaws.com/dev/encrypt',
+    headers : {
+      'content-type' : 'application/json'
+    },
+    body : req
+  }
+  request.post(reqSth, function(err, response, body){
+    var cipher = JSON.parse(response.body).ciphertext;
+    if(cipher === undefined)
+      retres.json(400, {
+        "message": "Bad Request",
+      });
+    if(cipher.length > 32)
+      return res.json(413, {
+        "message": "Too Long",
+      })
+    return res.json(200, {
+      "ciphertext" : cipher
+    })
   })
 }
 
 module.exports = {
-  encrypt
+  encrypt,
 }
